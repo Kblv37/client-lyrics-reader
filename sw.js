e69fb9d2c4beae1b1465b32e0b6a3e13bbb09617
Johongir <162979240+Kblv37@users.noreply.github.com>
2026-02-08 17:03:31 +0500

    PWA updated

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "rr-lyrics-v1";
+const CACHE_NAME = "rr-lyrics-v3";
 
-const CORE_ASSETS = [
+const CORE = [
   "/",
   "/index.html",
   "/music/index.html",
@@ -8,23 +8,19 @@ const CORE_ASSETS = [
 ];
 
 /* INSTALL */
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(CORE_ASSETS);
-    })
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE))
   );
 });
 
 /* ACTIVATE */
-self.addEventListener("activate", event => {
-  event.waitUntil(
+self.addEventListener("activate", e => {
+  e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
         })
       )
     )
@@ -33,6 +29,28 @@ self.addEventListener("activate", event => {
 
 /* FETCH */
 self.addEventListener("fetch", event => {
+
+  const url = new URL(event.request.url);
+
+  /* === NAVIGATION REQUESTS === */
+  if (event.request.mode === "navigate") {
+
+    // Если переход в /music
+    if (url.pathname.startsWith("/music")) {
+      event.respondWith(
+        caches.match("/music/index.html")
+      );
+      return;
+    }
+
+    // Все остальные страницы
+    event.respondWith(
+      caches.match("/index.html")
+    );
+    return;
+  }
+
+  /* === ASSETS / TXT === */
   event.respondWith(
     caches.match(event.request).then(res => {
       return res || fetch(event.request).then(fetchRes => {
@@ -43,4 +61,5 @@ self.addEventListener("fetch", event => {
       });
     })
   );
+
 });
